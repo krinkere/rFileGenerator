23af3362adc2d8f96aaf3ef9d97a236e601e54f4
Al <al.krinker@gmail.com>
2014-04-28 23:42:01 -0400

    Initial commit

---
+++ src/main/java/com/blogpost/alkrinker/rfilegenerator/App.java
@@ -0,0 +1,13 @@
+package com.blogpost.alkrinker.rfilegenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
